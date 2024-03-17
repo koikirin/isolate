c75ae9b20533121a73dcf8c9f813aef8307ea090
Hieuzest <girkirin@hotmail.com>
2024-03-17 22:47:20 +0800

    feat: basic impl

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-isolate?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-isolate)
 
-Create isolated context for plugin
+Create isolated context for plugin group
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,30 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'isolate'
 
-export interface Config {}
+export const filter = false
 
-export const Config: Schema<Config> = Schema.object({})
+export const reusable = true
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export interface Config {
+  isolatedServices: string[]
+}
+
+export const Config: Schema<Config> = Schema.object({
+  isolatedServices: Schema.array(String).role('table').default([]),
+})
+
+const kRecord = Symbol.for('koishi.loader.record')
+
+export function apply(_ctx: Context, _config: Config) {
+  const config = _ctx.scope.parent.config
+  const disabled = Object.keys(config).filter(key => key.startsWith('~') && !key.startsWith('~isolate:'))
+
+  let ctx = _ctx
+  _config.isolatedServices.forEach(name => ctx = ctx.isolate(name))
+  ctx.scope[kRecord] = ctx.root.scope[kRecord]
+
+  disabled.forEach(key => {
+    _ctx.logger.info('apply isolated plugin %c', key.slice(1))
+    ctx.loader.reload(ctx, key.slice(1), config[key])
+  })
 }
